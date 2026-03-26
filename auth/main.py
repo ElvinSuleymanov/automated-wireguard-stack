19e6d2f193021ec191f0688e06891f402547d100
ElvinSuleymanov <elvinsuleymanov150@gmail.com>
2026-03-26 13:46:39 +0400

    changed order of dependency installation in dockerfile of auth service

--- auth/main.py
+++ auth/main.py
@@ -3,18 +3,21 @@ import requests;
 import subprocess
 from fastapi import FastAPI, Security
 from helpers import verify
-app = FastAPI()
 
- 
+app = FastAPI()
+ip_sidecar = os.environ["IP_SIDECAR"]
+port_sidecar = os.environ["PORT_SIDECAR"]
+pubkey = os.environ["SERVER_PUBLIC_KEY"]
+
 @app.get("/health")
 def health_check():
     try:
-        subprocess.run(["wg show wg0"], check=True, capture_output=True)
+        subprocess.run(["wg", "show", "wg0"], check=True, capture_output=True)
         return {"status":"ok"}
     except:
         return {"status":"wg0 is not ready"}
 
 @app.get("/", dependencies=[Security(verify)])
 def authenticate_client_script():
-    requests.post()
-    return {"public_key": os.environ["SERVER_PUBLIC_KEY"]}
+    requests.post(f"http://{ip_sidecar}:{port_sidecar}", {})
+    return {"public_key": pubkey}
